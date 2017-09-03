744926af1e470776bbcec64319052eb2def8ab08
harpreet97 <harrycheema97@yahoo.com>
2017-09-04 11:34:20 +1200

    intial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+//java script code
+alert("This progrm will ask you to enter the two numbers");
+
+let number1 = prompt("Please type in the number 1", "00");
+console.log("number1 = " + number1);
+
+
+
+let number2 = prompt("Please type in the number 2");
+console.log("number2 = " + number2);
+
+if(number1 > number2){
+    console.log("number1 is greater than number2");
+}
+else if(number1 == number2){
+    console.log("Both numbers are equal");
+}
+else{
+    console.log("number2 is greater than number1");
+}
+
+
+
